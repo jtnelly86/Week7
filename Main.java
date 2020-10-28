6328d7a8fdd2ba34f18bf70c82c07f7a06939054
jtnelly86 <63426445+jtnelly86@users.noreply.github.com>
2020-10-28 13:19:07 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Employee e1 = new Employee(13);
+		Employee e2 = new Employee(14);
+		Employee e3 = new Employee(15);
+		
+		
+		e1.printEmployee();
+		e2.printEmployee();
+		e3.printEmployee();
+
+	}
+
+}
